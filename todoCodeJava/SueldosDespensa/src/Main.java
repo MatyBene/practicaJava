924b2faa3272af3c196690860664855b142ab40e
MatyBene <benedettimaty@gmail.com>
2024-07-05 14:18:20 -0300

    agrega ejercicio de condicionales

---
+++ todoCodeJava/SueldosDespensa/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double sueldo;
+        int categoria;
+
+        System.out.println("Ingrese el tipo de categoria que desea calcular el sueldo: ");
+        Scanner teclado = new Scanner(System.in);
+        categoria = teclado.nextInt();
+
+        switch (categoria) {
+            case 1:
+        }
+    }
+}
