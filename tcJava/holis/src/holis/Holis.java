12c5e905f272e34e345ef102ddc4330f29f8b2de
MatyBene <benedettimaty@gmail.com>
2024-07-08 13:08:11 -0300

    renombra carpeta. realiza prueba de interfaz grafica

---
+++ tcJava/holis/src/holis/Holis.java
@@ -0,0 +1,14 @@
+package holis;
+
+import igu.Pantalla;
+
+public class Holis {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
