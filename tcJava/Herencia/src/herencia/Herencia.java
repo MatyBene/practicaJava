79de2b71ace5e1b743f780620e78b3a419161e9f
MatyBene <benedettimaty@gmail.com>
2024-07-09 19:28:56 -0300

    implementa concepto polimorfismo

--- tcJava/Herencia/src/herencia/Herencia.java
+++ tcJava/Herencia/src/herencia/Herencia.java
@@ -4,15 +4,35 @@ public class Herencia {
 
     public static void main(String[] args) {
 
-        Empleado emple = new Empleado();
+        /* HERENCIA */
         
-        emple.getNumLegajo(); // METODO PROPIO
-        emple.getApellido(); // METODO HEREDADO DE PERSONA
+//        Empleado emple = new Empleado();
+//        
+//        emple.getNumLegajo(); // METODO PROPIO
+//        emple.getApellido(); // METODO HEREDADO DE PERSONA
+//        
+//        Consultor consul = new Consultor();
+//        
+//        consul.getNumConsultor(); // METODO PROPIO
+//        consul.getApellido(); // METODO HEREDADO DE PERSONA
         
-        Consultor consul = new Consultor();
+        /* -------- */
         
-        consul.getNumConsultor(); // METODO PROPIO
-        consul.getApellido(); // METODO HEREDADO DE PERSONA
+        /* POLIMORFISMO */
+        
+        // Son formas diferentes de representar a una persona, por eso las puedo guardar en un vector de tipo Persona
+        Persona vector[] = new Persona[5];
+        vector[0] = new Persona();
+        vector[1] = new Empleado();
+        vector[2] = new Consultor();
+        vector[3] = new Jefe();
+        
+        Persona persona = new Persona();
+        Consultor consultor = new Consultor();
+        
+        persona = consultor; // como consultor es hijo de la clase persona, se pueden hacer estas asignaciones
+        
+        /* -------- */
     }
     
 }
--- tcJava/Herencia/src/herencia/Jefe.java
+++ tcJava/Herencia/src/herencia/Jefe.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package herencia;
 
-/**
- *
- * @author Usuario
- */
-public class Jefe {
+public class Jefe extends Persona{
+    
+    int idJefe;
+    String departamentoJefe;
+
+    public Jefe() {
+    }
+
+    public Jefe(int idJefe, String departamentoJefe, int id, String dni, String nombre, String apellido, String domicilio, String telefono) {
+        super(id, dni, nombre, apellido, domicilio, telefono);
+        this.idJefe = idJefe;
+        this.departamentoJefe = departamentoJefe;
+    }
+
+    public int getIdJefe() {
+        return idJefe;
+    }
+
+    public void setIdJefe(int idJefe) {
+        this.idJefe = idJefe;
+    }
+
+    public String getDepartamentoJefe() {
+        return departamentoJefe;
+    }
+
+    public void setDepartamentoJefe(String departamentoJefe) {
+        this.departamentoJefe = departamentoJefe;
+    }
+    
     
 }
